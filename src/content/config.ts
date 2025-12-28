8f60c802f0b1f4b0cc907e5e81cea3f588627ef2
Jake Stewart <42218259+JakeyStew@users.noreply.github.com>
2025-12-28 18:59:02 +0000

    update: Config

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { defineCollection, z } from 'astro:content';
+
+const projects = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    slug: z.string(),
+    featuredImage: z.string(),
+    gallery: z.array(z.string()),
+    description: z.string(),
+  }),
+});
+
+export const collections = {
+  projects,
+};
