390d80f62f7f0d566931ea462827715bcaee9254
Jake Stewart <42218259+JakeyStew@users.noreply.github.com>
2026-01-01 18:26:04 +0000

    Update: Generic

---
+++ src/consts.ts
@@ -0,0 +1,4 @@
+// Gloabl data file
+
+export const SITE_TITLE = "Equine Photography | Jack Flash Photography";
+export const SITE_DESC = "Professional equine photography by Jack Flash Photography, showcasing elegant horse portraits and dynamic equestrian action imagery."
--- src/content/projects/example.md
+++
@@ -1,8 +0,0 @@
----
-title: "Example Project"
-featuredImage: "src/images/portfolio/images.png"
-gallery:
-  - "src/images/portfolio/images-1.png"
-  - "src/images/portfolio/images-2.png"
-description: "This is a test project for the portfolio."
----
