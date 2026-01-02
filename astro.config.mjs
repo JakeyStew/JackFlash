33fe9e7054f23846ef46b8963c2f66a9f15675a5
Jake Stewart <42218259+JakeyStew@users.noreply.github.com>
2026-01-02 15:43:35 +0000

    Update: Socials and setup of pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  site: 'http://localhost:4321',  // Update to site domain once Live
+  integrations: [react(), mdx(), sitemap()],
 });
