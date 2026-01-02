0a63f78420ed0ed81751b15e07b70eea70a4aae9
Jake Stewart <42218259+JakeyStew@users.noreply.github.com>
2026-01-02 19:21:54 +0000

    Update: Footer again

---
+++ src/config/footer-gallery.config.ts
@@ -0,0 +1,14 @@
+// src/config/footer-gallery.config.ts
+export default {
+  images: [
+    {
+      image: "/assets/images/footer/image1.png",
+    },
+    {
+      image: "/assets/images/footer/image2.png",
+    },
+    {
+      image: "/assets/images/footer/image3.png",
+    }
+  ]
+};
